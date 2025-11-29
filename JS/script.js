d82f8840951291097a2152ad006ce9d86d18d62c
XLexYzz <xlexyzz@gmail.com>
2025-11-29 18:29:30 +0100

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 
 function descargarBanlist() {
     // En lugar de tener el contenido hardcodeado, ahora apunta a tu archivo
-    const url = '../banlist/!XYZ banlist.lflist.conf';
+    const url = '../banlist/Untitled Banlist.lflist.conf';
     
     // Crear un enlace temporal para la descarga
     const a = document.createElement('a');
